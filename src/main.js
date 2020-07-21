bed1d60027c45a6f0189853189e5515d697b0f17
Kevin <grj@bwg.art>
2020-07-21 14:54:08 +0800

    add axios updata: utils

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false;
 
@@ -11,6 +12,7 @@ import utils from './utils'
 // 挂载公共js到Vue
 Vue.prototype.config = config;
 Vue.prototype.utils = utils;
+Vue.prototype.$axios = axios;
 
 // 引入公共组件
 import Header from './components/Header'
